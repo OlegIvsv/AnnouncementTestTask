8be48e1dd166fa1ad83b56d6c16e84c6965266cf
Oleh Ivasiv <o.ivasiv@abtollc.com>
2023-06-20 16:23:26 +0300

    Add repository interface

---
+++ AnnouncementService.Infrastructure/Database/IAnnouncementRepo.cs
@@ -0,0 +1,13 @@
+﻿using AnnouncementService.App.Entities;
+using FluentResults;
+
+namespace AnnouncementService.Infrastructure.Database;
+
+public interface IAnnouncementRepo
+{
+    public Task<Result<Announcement>> GetById(Guid id);
+    public Task<bool> Delete(Guid id);
+    public Task<Result<Announcement>> Update(Announcement announcement);
+    public Task<Result<Announcement>> Add(Announcement announcement);
+    public Task<Result<IList<Announcement>>> GetSimilar(Guid id, int n);
+}
---
+++ AnnouncementService.Infrastructure/Database/RedisAnnouncementRepo.cs
@@ -0,0 +1,32 @@
+﻿using AnnouncementService.App.Entities;
+using FluentResults;
+
+namespace AnnouncementService.Infrastructure.Database;
+
+public class RedisAnnouncementRepo : IAnnouncementRepo
+{
+    public Task<Result<Announcement>> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<Announcement>> Update(Announcement announcement)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<Announcement>> Add(Announcement announcement)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<IList<Announcement>>> GetSimilar(Guid id, int n)
+    {
+        throw new NotImplementedException();
+    }
+}
