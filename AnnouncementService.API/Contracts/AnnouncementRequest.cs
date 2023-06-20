0912759d405a8df4e3bddcd78d6fd61dcce718dc
Oleh Ivasiv <o.ivasiv@abtollc.com>
2023-06-20 16:56:09 +0300

    Add contract models

---
+++ AnnouncementService.API/Contracts/AnnouncementRequest.cs
@@ -0,0 +1,22 @@
+﻿using AnnouncementService.App.Entities;
+
+namespace AnnouncementService.API.Contracts;
+
+public class AnnouncementRequest
+{
+    public Guid Id { get; init; }
+    public string Title { get; init; }
+    public string Description { get; init; }
+    public DateTime DateAdded { get; init; }
+
+    public static Announcement ToModel(AnnouncementRequest announcement)
+    {
+        return new()
+        {
+            Id = announcement.Id,
+            Title = announcement.Title,
+            Description = announcement.Description,
+            DateAdded = announcement.DateAdded
+        };
+    }
+}
---
+++ AnnouncementService.API/Contracts/AnnouncementResponse.cs
@@ -0,0 +1,22 @@
+﻿using AnnouncementService.App.Entities;
+
+namespace AnnouncementService.API.Contracts;
+
+public class AnnouncementResponse
+{
+    public Guid Id { get; init; }
+    public string Title { get; init; }
+    public string Description { get; init; }
+    public DateTime DateAdded { get; init; }
+
+    public static AnnouncementResponse FromModel(Announcement announcement)
+    {
+        return new()
+        {
+            Id = announcement.Id,
+            Title = announcement.Title,
+            Description = announcement.Description,
+            DateAdded = announcement.DateAdded
+        };
+    }
+}
