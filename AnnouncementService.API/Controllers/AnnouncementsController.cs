d8c5f045daea52aa43850f72ad1b8914fd25cf16
Oleh Ivasiv <o.ivasiv@abtollc.com>
2023-06-20 17:06:11 +0300

    Add basic controller implementation

--- AnnouncementService.API/Controllers/AnnouncementsController.cs
+++ AnnouncementService.API/Controllers/AnnouncementsController.cs
@@ -1,3 +1,6 @@
+using AnnouncementService.API.Contracts;
+using AnnouncementService.App.Entities;
+using AnnouncementService.Infrastructure.Database;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AnnouncementService.API.Controllers;
@@ -6,33 +9,58 @@ namespace AnnouncementService.API.Controllers;
 [Route("[controller]")]
 public class AnnouncementsController : ControllerBase
 {
-    [HttpPost]
-    public async Task<IActionResult> AddAnnouncement()
+    private readonly IAnnouncementRepo _repo;
+
+    public AnnouncementsController(IAnnouncementRepo announcementRepo)
     {
-        
+        _repo = announcementRepo;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAnnouncement([FromBody] AnnouncementRequest request)
+    {
+        var announcement = AnnouncementRequest.ToModel(request);
+        _repo.Add(announcement);
+        return Ok();
     }
     
-    [HttpDelete]
-    public async Task<IActionResult> RemoveAnnouncement()
+    [HttpDelete("{id:Guid}")]
+    public async Task<IActionResult> RemoveAnnouncement(Guid id)
     {
-        
+        var announcement = await _repo.GetById(id);
+        if (announcement is null)
+            return NotFound();
+        await _repo.Delete(id);
+        return Ok();
     }
 
     [HttpPut]
-    public async Task<IActionResult> UpdateAnnouncement()
+    public async Task<IActionResult> UpdateAnnouncement([FromBody] AnnouncementRequest request)
     {
-
+        var existingAnnouncement = await _repo.GetById(request.Id);
+        if (existingAnnouncement is null)
+            return NotFound();
+        var announcement = AnnouncementRequest.ToModel(request);
+        await _repo.Update(announcement);
+        return Ok();
     }
 
     [HttpGet("{id:Guid}")]
     public async Task<IActionResult> GetAnnouncementById(Guid id)
     {
-
+        var announcement = await _repo.GetById(id);
+        if (announcement is null)
+            return NotFound();
+        return Ok(announcement);
     }
 
     [HttpGet("similar-to/{id}")]
     public async Task<IActionResult> GetSimilar(Guid id, [FromQuery] int length)
     {
-        
+        var existingAnnouncement = await _repo.GetById(id);
+        if (existingAnnouncement is null)
+            return BadRequest();
+        var listOfSimilar = await _repo.GetSimilar(id, length);
+        return Ok(listOfSimilar);
     }
 }
